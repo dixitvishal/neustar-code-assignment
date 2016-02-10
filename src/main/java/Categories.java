5009ad2624a079a675fe15fd370e2b6efd64b63c
vdixit <vishal@graph.us>
2016-02-09 20:26:34 -0500

    Formatted the code

--- src/main/java/Categories.java
+++ src/main/java/Categories.java
@@ -4,72 +4,69 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * This class encapsulates the valid categories present in the input file.
- * Valid categories are PERSON, PLACE, ANIMAL, COMPUTER and OTHER. It also
- * contains an order in which the categories are to be displayed.
+ * This class encapsulates the valid categories present in the input file. Valid
+ * categories are PERSON, PLACE, ANIMAL, COMPUTER and OTHER. It also contains an
+ * order in which the categories are to be displayed.
  *
  */
 public enum Categories {
-	
-	PERSON("PERSON", 0),
-	PLACE("PLACE", 1),
-	ANIMAL("ANIMAL", 2),
-	COMPUTER("COMPUTER", 3),
-	OTHER("OTHER", 4);
-	
+
+	PERSON("PERSON", 0), PLACE("PLACE", 1), ANIMAL("ANIMAL", 2), COMPUTER("COMPUTER", 3), OTHER("OTHER", 4);
+
 	private String value;
-	
+
 	private int order;
-	
+
 	/**
 	 * Constructor
 	 * 
 	 * @param value
 	 * @param order
 	 */
-    private Categories(String value, int order) {
-            this.value = value;
-            this.order = order;
-    }
-    
-    /**
+	private Categories(String value, int order) {
+		this.value = value;
+		this.order = order;
+	}
+
+	/**
 	 * Retrieves a set of the display order values for the categories
 	 * 
 	 * @return set of order values
 	 */
-    public static Set<Integer> getCategoryOrders() {
-    	Set<Integer> values = new HashSet<Integer>();
+	public static Set<Integer> getCategoryOrders() {
+		Set<Integer> values = new HashSet<Integer>();
 		for (Categories category : EnumSet.allOf(Categories.class)) {
-    		values.add(category.order);    	
-    	}
-    	return values;   	
-    }
-    
-    /**
+			values.add(category.order);
+		}
+		return values;
+	}
+
+	/**
 	 * Retrieves a set of all category values
 	 * 
 	 * @return set of category values
 	 */
-    public static Set<String> getCategoryValues() {
-    	Set<String> values = new TreeSet<String>();
+	public static Set<String> getCategoryValues() {
+		Set<String> values = new TreeSet<String>();
 		for (Categories category : EnumSet.allOf(Categories.class)) {
-    		values.add(category.value);    	
-    	}
-    	return values;   	
-    }
-    
-    /**
+			values.add(category.value);
+		}
+		return values;
+	}
+
+	/**
 	 * Retrieves a category value given it's display order
 	 * 
-	 * @param order of display
+	 * @param order
+	 *            of display
 	 * @return category values
 	 */
-    public static String getCategoryValue(int order) {
+	public static String getCategoryValue(int order) {
 		for (Categories category : EnumSet.allOf(Categories.class)) {
 			if (order == category.order) {
 				return category.value;
 			}
-    	}
+		}
 		return null;
-    }
+	}
 }
--- src/main/java/InputFileReader.java
+++ src/main/java/InputFileReader.java
@@ -4,48 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * class to read line by line from a file.
+ * Class to read line by line from a file.
  *
  */
 public class InputFileReader {
-	
+
 	/**
-	 * Fully qualified name of the input file, right now to
-	 * simplify things all files are kept in the resources folder
+	 * Fully qualified name of the input file, right now to simplify things all
+	 * files are kept in the resources folder
 	 */
-    private String fileName;
-    
-    /**
+	private String fileName;
+
+	/**
 	 * Method for returning a list of lines present in the input file
 	 * 
 	 * @return List of lines
 	 */
-    public List<String> getLines() {
-	    try {
-	    	List<String> lines = new ArrayList<String>();
-	    	
-	        FileReader fileReader = new FileReader(fileName);
-	        BufferedReader bufferedReader = new BufferedReader(fileReader);
-	        
-	        String line = null;
-	        while((line = bufferedReader.readLine()) != null) {
-	        	lines.add(line);	        
-	        }   
-	        bufferedReader.close();  
-	        return lines;
-	    } catch(Exception e) {
-	        System.out.println("Exception in reading the input file " + 
-	            fileName); 
-	        return null;
-	    } 
-    }
-    
-    /**
+	public List<String> getLines() {
+		try {
+			List<String> lines = new ArrayList<String>();
+
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
+				lines.add(line);
+			}
+			bufferedReader.close();
+			return lines;
+		} catch (Exception e) {
+			System.out.println("Exception in reading the input file " + fileName);
+			return null;
+		}
+	}
+
+	/**
 	 * Setter for fileName attribute
 	 * 
 	 * @param fileName
 	 */
-    public void setFileName(String fileName) {
-    	this.fileName = fileName;
-    }
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 }
